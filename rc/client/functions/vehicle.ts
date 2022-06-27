fb547189a3f8929533bd67aa6391b464c60cf39f
niiyy <smailaberkaoui@gmail.com>
2022-06-27 13:30:02 +0200

    init

---
+++ rc/client/functions/vehicle.ts
@@ -0,0 +1,27 @@
+
+
+
+'qzdqzdqzdqzd' // string
+182383 // number
+true // boolean
+
+
+class Vehicle {
+    constructor() {}
+
+
+    Spawn(vehicle: string) {
+        // code !
+    }
+
+    DeleteObject() {
+        // code !
+    }
+
+}
+
+// NAF.Vehicle.Spawn('t20')
+
+
+export default Vehicle
+
---
+++ rc/webpack.config.js
@@ -0,0 +1,56 @@
+const webpack = require('webpack');
+const path = require('path');
+const buildPath = path.resolve(__dirname, 'dist');
+
+const server = () => {
+  return {
+    entry: './server/index.ts',
+    target: 'node',
+    module: {
+      rules: [
+        {
+          test: /\.ts?$/,
+          use: ['ts-loader'],
+          exclude: /node_modules/,
+        },
+      ],
+    },
+    optimization: {
+      minimize: true,
+    },
+    resolve: {
+      extensions: ['.ts'],
+    },
+    output: {
+      filename: 'server.min.js',
+      path: path.resolve(buildPath, 'server'),
+    },
+  };
+};
+
+const client = () => {
+  return {
+    entry: './client/index.ts',
+    module: {
+      rules: [
+        {
+          test: /\.ts?$/,
+          use: ['ts-loader'],
+          exclude: /node_modules/,
+        },
+      ],
+    },
+    optimization: {
+      minimize: true,
+    },
+    resolve: {
+      extensions: ['.ts'],
+    },
+    output: {
+      filename: 'client.min.js',
+      path: path.resolve(buildPath, 'client'),
+    },
+  };
+};
+
+module.exports = [server, client];
