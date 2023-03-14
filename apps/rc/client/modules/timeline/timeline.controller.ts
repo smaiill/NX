c597616d6286f9914c8f4619c143444a9751166c
niiyy <smailaberkaoui@gmail.com>
2023-03-14 18:15:08 +0100

    removed default ons

--- apps/rc/client/modules/timeline/timeline.controller.ts
+++ apps/rc/client/modules/timeline/timeline.controller.ts
@@ -3,18 +3,6 @@ import { TimelineEvents } from '@nx/types'
 import { CreateTimelineType, UpdateTimelineType } from './timeline.schema'
 import { TimelineService } from './timeline.service'
 
-on(TimelineEvents.CREATE_TIMELINE, (timeline: CreateTimelineType): void => {
-  TimelineService.create(timeline)
-})
-
-on(TimelineEvents.UPDATE_TIMELINE, (timeline: UpdateTimelineType): void => {
-  TimelineService.update(timeline)
-})
-
-on(TimelineEvents.DESTROY_TIMELINE, (): void => {
-  TimelineService.destroy()
-})
-
 export class TimelineController {
   @On(TimelineEvents.CREATE_TIMELINE)
   public handleCreateTimeline(timeline: CreateTimelineType) {
